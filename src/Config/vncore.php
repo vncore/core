9b49798153a8985ceff3186fdedb6485f63bef6e
vncore <vncore.net@gmail.com>
2024-09-15 08:33:49 +0700

    Update core version

--- src/Config/vncore.php
+++ src/Config/vncore.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'core'                 => '1.0',
-    'core-sub-version'     => '1.0.2',
+    'core-sub-version'     => '1.0.3',
     'homepage'             => 'https://vncore.net',
     'name'                 => 'Core laravel admin for all systems',
     'github'               => 'https://github.com/vncore/core',
